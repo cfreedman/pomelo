a9d265be36605d3089555f2f24df8bf57796c220
Collum <collum.freedman@gmail.com>
2025-05-01 21:02:59 -0400

    add shopping list routes

--- pomelo-server/app/routes/shopping_list.py
+++ pomelo-server/app/routes/shopping_list.py
@@ -1,19 +1,26 @@
-from flask import Blueprint, make_response, request
+from flask import Blueprint, jsonify, make_response, request
 
 from app import db
 from app.models import ShoppingList
+import app.schema.shopping_list as schema
 
 shopping_list_bp = Blueprint("shopping-list", __name__)
 
 
 @shopping_list_bp.get("/<int:id>")
 def get_shopping_list_by_id(id: int):
-    pass
+    shopping_list = ShoppingList.query.get_or_404(id)
+
+    response = schema.ShoppingList.model_validate(**shopping_list).model_dump()
+    return jsonify(response), 201
 
 
 @shopping_list_bp.post("/")
 def create_shopping_list():
     data = request.get_json()
+    shopping_list_data = schema.ShoppingListCreate.model_validate(**data)
+
+    new_shopping_list = ShoppingList()
 
 
 @shopping_list_bp.put("/<int:id>")
