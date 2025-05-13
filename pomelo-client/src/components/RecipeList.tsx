ce2a11fab81f76e36f830f619da0c6e5a9c224c6
Collum <collum.freedman@gmail.com>
2025-05-13 17:40:17 -0400

    random changes

--- pomelo-client/src/components/RecipeList.tsx
+++ pomelo-client/src/components/RecipeList.tsx
@@ -1,5 +1,6 @@
 import { JSX, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
+import { ChevronRight, ChevronLeft } from "lucide-react";
 
 import { Button } from "./ui/button";
 import {
@@ -15,6 +16,15 @@ import { Table, TableBody, TableCell, TableHeader, TableRow } from "./ui/table";
 import TagIcon from "./TagIcon";
 import { Recipe, fetchAllRecipes } from "@/lib/recipes";
 import { DUMMY_RECIPES } from "@/dummy/recipes";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "./ui/pagination";
 
 export default function RecipeList(): JSX.Element {
   const { data: recipes, isLoading } = useQuery<Recipe[]>({
@@ -24,6 +34,7 @@ export default function RecipeList(): JSX.Element {
     initialData: DUMMY_RECIPES,
   });
   const [filterOpen, setFilterOpen] = useState(false);
+  const [itemsPerPage, setItemsPerPage] = useState(15);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -43,90 +54,124 @@ export default function RecipeList(): JSX.Element {
     )
   );
 
+  const pages = Math.round(recipes.length / itemsPerPage);
+
   return (
-    <div className="flex flex-col grow px-[30px] py-[50px]">
-      <h1>Recipes</h1>
-      <Button className="w-[80px] " onClick={() => setFilterOpen(!filterOpen)}>
-        Filters
-      </Button>
-      {filterOpen && (
-        <div className="flex my-2 p-2 bg-gray-100 rounded-sm">
-          <div className="flex flex-row flex-wrap mx-2">
-            {cuisines.map((cuisine) => (
-              <Button className="mx-1" key={cuisine}>
-                {cuisine}
-              </Button>
-            ))}
+    <div className="flex flex-col w-full">
+      <div className="flex flex-col grow px-[30px] py-[50px]">
+        <h1>Recipes</h1>
+        <Button
+          className="w-[80px] "
+          onClick={() => setFilterOpen(!filterOpen)}
+        >
+          Filters
+        </Button>
+        {filterOpen && (
+          <div className="flex my-2 p-2 bg-gray-100 rounded-sm">
+            <div className="flex flex-row flex-wrap mx-2">
+              {cuisines.map((cuisine) => (
+                <Button className="mx-1" key={cuisine}>
+                  {cuisine}
+                </Button>
+              ))}
+            </div>
+            <Select>
+              <SelectTrigger>
+                <SelectValue placeholder="Group by" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Cuisine</SelectLabel>
+                  {cuisines.map((cuisine) => (
+                    <SelectItem key={cuisine} value="cuisine">
+                      {cuisine}
+                    </SelectItem>
+                  ))}
+                  <SelectLabel>Meal Type</SelectLabel>
+                  {mealTypes.map((type) => (
+                    <SelectItem key={type} value={type}>
+                      {type}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
           </div>
-          <Select>
-            <SelectTrigger>
-              <SelectValue placeholder="Group by" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectLabel>Cuisine</SelectLabel>
-                {cuisines.map((cuisine) => (
-                  <SelectItem key={cuisine} value="cuisine">
-                    {cuisine}
-                  </SelectItem>
-                ))}
-                <SelectLabel>Meal Type</SelectLabel>
-                {mealTypes.map((type) => (
-                  <SelectItem key={type} value={type}>
-                    {type}
-                  </SelectItem>
-                ))}
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </div>
-      )}
-      <Table className="my-2 px-3">
-        <TableHeader>
-          <TableRow className="hover:bg-white">
-            <TableCell className="text-left font-bold text-xl text-breaker-bay-600">
-              Title
-            </TableCell>
-            <TableCell className="text-left font-bold text-xl text-breaker-bay-600">
-              Tags
-            </TableCell>
-            <TableCell className="text-left font-bold text-xl text-breaker-bay-600">
-              Dish Type
-            </TableCell>
-            <TableCell className="text-right font-bold text-xl text-breaker-bay-600">
-              Cuisine
-            </TableCell>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {recipes?.map((recipe) => (
-            <TableRow
-              key={recipe.name}
-              className="hover:bg-breaker-bay-200 even:bg-breaker-bay-100 border-none"
-            >
-              <TableCell className="text-left text-lg py-5">
-                {recipe.name}
+        )}
+        <Table className="my-2 px-3">
+          <TableHeader>
+            <TableRow className="hover:bg-white">
+              <TableCell className="text-left font-bold text-xl text-breaker-bay-600">
+                Title
               </TableCell>
-              <TableCell className="text-left text-lg py-5 flex gap-1">
-                {recipe.tags.map(({ name }) => (
-                  <TagIcon
-                    key={name}
-                    name={name.toLowerCase()}
-                    width={30}
-                    height={30}
-                  />
-                ))}
+              <TableCell className="text-left font-bold text-xl text-breaker-bay-600">
+                Tags
               </TableCell>
-              <TableCell className="text-left text-lg py-5">
-                {recipe.mealType}
+              <TableCell className="text-left font-bold text-xl text-breaker-bay-600">
+                Dish Type
               </TableCell>
-              <TableCell className="text-right text-lg py-5">
-                {recipe.cuisine}
+              <TableCell className="text-right font-bold text-xl text-breaker-bay-600">
+                Cuisine
               </TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHeader>
+          <TableBody>
+            {recipes?.map((recipe) => (
+              <TableRow
+                key={recipe.name}
+                className="hover:bg-breaker-bay-200 even:bg-breaker-bay-100 border-none"
+              >
+                <TableCell className="text-left text-lg py-5">
+                  {recipe.name}
+                </TableCell>
+                <TableCell className="text-left text-lg py-5 flex gap-1">
+                  {recipe.tags.map(({ name }) => (
+                    <TagIcon
+                      key={name}
+                      name={name.toLowerCase()}
+                      width={30}
+                      height={30}
+                    />
+                  ))}
+                </TableCell>
+                <TableCell className="text-left text-lg py-5">
+                  {recipe.mealType}
+                </TableCell>
+                <TableCell className="text-right text-lg py-5">
+                  {recipe.cuisine}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="mt-4 flex w-full gap-4 items-center">
+        <select>
+          <option value={10}>10</option>
+          <option value={20}>20</option>
+          <option value={50}>50</option>
+          <option value={100}>100</option>
+        </select>
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious onClick={() => console.log("Poop")} />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink>1</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink>2</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationNext />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </div>
     </div>
   );
 }
