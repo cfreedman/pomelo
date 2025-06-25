3efce0a90bffd079e3234cc398e17f75ec7a88db
Collum Freedman <collum.freedman@gmail.com>
2025-06-25 12:54:12 -0400

    add recipe message producer for kafka into main flask app

--- app/app/routes/recipes.py
+++ app/app/routes/recipes.py
@@ -1,8 +1,11 @@
+import json
+import uuid
 from flask import Blueprint, jsonify, request
 from sqlalchemy import select
 
 from app.extensions import db
 from app.models import Recipe
+from app.producer import RecipeMessageProducer
 import app.schema.recipes as schema
 from app.services import create_new_recipe, update_existing_recipe
 
@@ -53,11 +56,19 @@ def create_recipe():
     data = request.get_json()
     recipe_data = schema.RecipeCreate.model_validate(data)
 
-    final_recipe = create_new_recipe(recipe_data, db)
+    created_recipe = create_new_recipe(recipe_data, db)
 
-    response = schema.Recipe.model_validate(
-        final_recipe.to_recipe_schema()
-    ).model_dump()
+    created_recipe = created_recipe.to_recipe_schema()
+    created_recipe_message = created_recipe.to_recipe_message()
+
+    producer = RecipeMessageProducer.get_instance()
+    producer.produce(
+        topic="create_recipe",
+        key=uuid.uuid4(),
+        value=json.dumps(created_recipe_message),
+    )
+
+    response = schema.Recipe.model_validate(created_recipe).model_dump()
     return jsonify(response), 201
 
 
@@ -68,15 +79,28 @@ def update_recipe_by_id(id: int):
     recipe = Recipe.query.get_or_404(id)
 
     updated_recipe = update_existing_recipe(recipe, recipe_data, db)
-    response = schema.Recipe.model_validate(
-        updated_recipe.to_recipe_schema()
-    ).model_dump()
+    updated_recipe = updated_recipe.to_recipe_schema()
+    updated_recipe_message = updated_recipe.to_recipe_message()
+
+    producer = RecipeMessageProducer.get_instance()
+    producer.produce(
+        topic="update_recipe",
+        key=uuid.uuid4(),
+        value=json.dumps(updated_recipe_message),
+    )
+
+    response = schema.Recipe.model_validate(updated_recipe).model_dump()
     return jsonify(response), 201
 
 
 @recipes_bp.delete("/<int:id>")
 def delete_recipe_by_id(id: int):
     recipe = Recipe.query.get_or_404(id)
+
     db.session.delete(recipe)
     db.session.commit()
+
+    producer = RecipeMessageProducer.get_instance()
+    producer.produce(topic="delete_recipe", key=uuid.uuid4(), value=str(recipe.id))
+
     return jsonify(f"Recipe {recipe.name} successfully deleted"), 200
--- app/app/schema/recipes.py
+++ app/app/schema/recipes.py
@@ -33,6 +33,20 @@ class Recipe(BaseModel):
     tags: List[Tag]
     ingredients: List[IngredientWithAmount]
 
+    def to_recipe_message(self):
+        ingredients = [ingredient.name for ingredient in self.ingredients]
+        tags = [tag.name for tag in self.tags]
+
+        return RecipeMessage(
+            id=self.id,
+            name=self.name,
+            cuisine=self.cuisine,
+            meal_type=self.meal_type,
+            servings=self.servings,
+            tags=tags,
+            ingredients=ingredients,
+        )
+
 
 class RecipeCreate(BaseModel):
     model_config = ConfigDict(from_attributes=True)
@@ -45,6 +59,16 @@ class RecipeCreate(BaseModel):
     servings: int
     tags: List[str]
     ingredients: List[IngredientWithAmountCreate]
+
+
+class RecipeMessage(BaseModel):
+    id: int
+    name: str
+    cuisine: Optional[str]
+    meal_type: Optional[str]
+    servings: int
+    tags: List[str]
+    ingredients: List[str]
 
 
 # Don't need just specify id in API hit
